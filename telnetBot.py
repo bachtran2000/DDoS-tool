5083cc0a31fb3e04b7454e09ca445ab1d1b36918
bachtran2000 <bachtran.2000@gmail.com>
2021-10-02 14:56:02 +0700

    add telnet

--- telnetBot.py
+++ telnetBot.py
@@ -1,11 +1,12 @@
 
 from telnetlib import Telnet
+import os
 
 class Telnet:
     def telnet(listip):
         for ip in listip:
             try:
-                tn = telnetlib.Telnet(ip)
+                os.system("telnet "+ip)
             except:
                 print('Could not connect. Exiting')
 
